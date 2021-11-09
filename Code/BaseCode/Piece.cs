d0a891d4be28d84b2f24793b1b21caf855f472a5
Timothy O <timoliver200@gmail.com>
2021-11-09 08:22:56 -0600

    added in GrabBag function and some changes to GetRandPiece

--- Code/BaseCode/Piece.cs
+++ Code/BaseCode/Piece.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿# pragma warning disable 1591
+using System;
 using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Quadris {
   public enum PieceType {
@@ -34,6 +37,7 @@ namespace Quadris {
     private const int LAYOUT_COLS = 4;
 
     private static readonly Random rand;
+    
 
     public bool[,] Layout { get; private set; }
     public int GridRow { get; set; }
@@ -41,37 +45,117 @@ namespace Quadris {
     public PieceType Type { get; private set; }
     public PieceColor Color { get; private set; }
 
-    static Piece() {
-      rand = new Random();
+    static Piece() 
+    {
+            rand = new Random();
+            
     }
 
-    public Piece(string strLayout, PieceColor color) {
-      GridRow = 0;
-      GridCol = 3;
-      Color = color;
-      Layout = new bool[LAYOUT_ROWS, LAYOUT_COLS];
-      for (int c = 0; c < LAYOUT_COLS; c++) {
-        for (int r = 0; r < LAYOUT_ROWS; r++) {
-          Layout[r, c] = (strLayout[r * LAYOUT_COLS + c] == '1');
+    public Piece(string strLayout, PieceColor color, PieceType type) 
+    {
+            GridRow = 0;
+            GridCol = 3;
+            Color = color;
+            Type = type;
+            
+            Layout = new bool[LAYOUT_ROWS, LAYOUT_COLS];
+            for (int c = 0; c < LAYOUT_COLS; c++) 
+            {
+                for (int r = 0; r < LAYOUT_ROWS; r++) 
+                {
+                    Layout[r, c] = (strLayout[r * LAYOUT_COLS + c] == '1');
+
+                }
+            }
         }
-      }
-    }
 
     public static Piece GetRandPiece() {
-      int pieceNum = rand.Next(Enum.GetValues(typeof(PieceType)).Length);
-      return MakePiece((PieceType)pieceNum);
+      int pieceNum = rand.Next(7);//Enum.GetValues(typeof(PieceType)).Length);
+      if (GrabBag(pieceNum))
+                return MakePiece((PieceType)pieceNum);
+      else
+                return GetRandPiece();
+      //Piece newPiece = MakePiece((PieceType)pieceNum);
+      //return MakePiece((PieceType)pieceNum);
+        /*if (GrabBag(newPiece))
+                return newPiece;
+        else
+                return GetRandPiece();
+        */
+        //return MakePiece((PieceType)pieceNum);
+      }
+
+      /**/
+        // checks if current pieceNum is in the list of available PieceTypes (by int value)
+        //   currently updates numbers correctly, but still allows already grabbed pieces to be made
+        //   --know numbers update correctly due to breakpoint--
+        public static bool GrabBag(int pieceNum)
+        {
+            //Console.WriteLine(Board.gBag.Count);
+            bool goodPiece = true;
+            if (Board.gBag.Count > 0)
+            {
+                if (Board.gBag.Contains(pieceNum))
+                {
+                    Board.gBag.Remove(pieceNum);
+                }
+
+                if (!(Board.gBag.Contains(pieceNum)))
+                {
+                    goodPiece = false;
+                    System.Diagnostics.Debug.WriteLine($"Can't make piece number {pieceNum}");
+
+                }
+            }
+            else
+            {
+                
+                Board.gBag = Board.PieceArr.ToList();
+                Board.gBag.Remove(pieceNum);
+            }
+            return goodPiece;
+        }
+    /*
+        // uses array implementation
+        public static Boolean GrabBag(Piece newPiece)
+    {
+      goodPiece = true;
+      for (int i = 0; i < currBag.length; i++)
+      {
+        if newPiece == currBag[i]
+          {
+            goodPiece = false;
+            break;
+          }
+        if currBag[i] == null
+        {
+          if (i != 6)
+          {
+          currbag[i] = newPiece;
+          }
+          else 
+          {
+            Array.Clear(currbag, 0, currbag.length)
+          }
+
+        }
+
+        
+      }
+      return goodPiece;
     }
+   /* */
 
     public static Piece MakePiece(PieceType type) {
       Piece piece = null;
       switch (type) {
-        case PieceType.L: piece = new Piece("0000010001000110", PieceColor.ORANGE); break;
-        case PieceType.J: piece = new Piece("0000001000100110", PieceColor.BLUE); break;
-        case PieceType.Z: piece = new Piece("0000011000110000", PieceColor.RED); break;
-        case PieceType.S: piece = new Piece("0000011011000000", PieceColor.GREEN); break;
-        case PieceType.I: piece = new Piece("0010001000100010", PieceColor.CYAN); break;
-        case PieceType.T: piece = new Piece("0000001001110000", PieceColor.PURPLE); break;
-        case PieceType.O: piece = new Piece("0000011001100000", PieceColor.YELLOW); break;
+        case PieceType.L: piece = new Piece("0000010001000110", PieceColor.ORANGE, PieceType.L); break;
+        case PieceType.J: piece = new Piece("0000001000100110", PieceColor.BLUE, PieceType.J); break;
+        case PieceType.Z: piece = new Piece("0000011000110000", PieceColor.RED, PieceType.Z); break;
+        case PieceType.S: piece = new Piece("0000011011000000", PieceColor.GREEN, PieceType.S); break;
+        case PieceType.I: piece = new Piece("0010001000100010", PieceColor.CYAN, PieceType.I); break;
+        case PieceType.T: piece = new Piece("0000001001110000", PieceColor.PURPLE, PieceType.T); break;
+        case PieceType.O: piece = new Piece("0000011001100000", PieceColor.YELLOW, PieceType.O); break;
       }
       return piece;
     }
